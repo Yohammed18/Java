c4ed5448464c42115231e3525beb2b89cf34042e
Mohammed Allibalogun <mallibalogun18@gmail.com>
2023-08-18 21:20:24 -0400

    Add all

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,12 @@
+import java.util.Arrays;
+
 public class HelloWorld {
 
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        int[] array = {4,2,0,19,-10};
+
+        System.out.println(Arrays.toString(array));
     }
 }
